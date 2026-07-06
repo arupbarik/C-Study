0439f444c0b65a13df077c99db4fed4b5b4bfb16
Arup Barik <arupbarik323@gmail.com>
2026-07-07 00:02:50 +0530

    sem material

---
+++ sem.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+// #include<math.h>
+// #include<stdlib.h>
+int main(){
+    printf("hello sem\n");
+    //Write a C program to count the number of even and odd elements in a 1-D array of n integers entered by the user.
+    // int n;
+    // printf("How many numbers do you want to enter?\n");
+    // scanf("%d",&n);
+    // int numbers[n];
+    // printf("please start\n");
+    // int count=0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     scanf("%d",&numbers[i]);
+    //     if (numbers[i]%2==0)
+    //     {
+    //         count++;
+    //     }
+        
+    // }
+    // for (int j = 0; j < n; j++)
+    // {
+    //     printf("%d\n",numbers[j]);
+    // }
+    // printf("No. of even is %d\n",count);
+    // printf("No. of odd is %d\n",n-count);
+
+    //Write a C program to find the second largest element in a 1-D array without sorting the array.
+    // int arsize;
+    // int largest;
+    // int secondlargest;
+    // printf("Enter which u prefer for an array\n");
+    // scanf("%d",&arsize);
+    // int arr[arsize];
+    // printf("input of array\n");
+    // for (int i = 0; i < arsize; i++)
+    // {
+    //     scanf("%d",&arr[i]);
+    // }
+    // if (arsize < 2)
+    // {
+    //     printf("Second largest element does not exist.\n");
+    // }    
+    // else
+    // {
+    //     if (arr[0] > arr[1])
+    // {
+    //     largest = arr[0];
+    //     secondlargest = arr[1];
+    // }
+    // else    return 0;
+    // {     
+    //     largest = arr[1];
+    //     secondlargest = arr[0];
+    // }
+    // for (int i = 2; i < arsize; i++)
+    // {
+    //     if (largest<arr[i])
+    //     {
+    //         secondlargest=largest;
+    //         largest=arr[i];
+    //     }
+    //     else if (secondlargest<arr[i] && largest>arr[i])
+    //     {
+    //         secondlargest=arr[i];
+    //     }
+    // }
+    // printf("the largest is %d\n", largest);
+    // printf("the secondlargest is %d\n",secondlargest);
+    // }
+    
+    //Write a C program to remove all duplicate elements from a 1-D array without using another array.
+    int size;
+    printf("Enter the size of array\n");
+    scanf("%d",&size);
+    int arrr[size];
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d",&arrr[i]);
+    }
+    
+    
+}
