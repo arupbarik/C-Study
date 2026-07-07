effcdd2d49ddc655ec14d63133c194c5ae2a500f
Arup Barik <arupbarik323@gmail.com>
2026-07-07 22:56:57 +0530

    prepared

--- sem.c
+++ sem.c
@@ -72,13 +72,45 @@ int main(){
     
     //Write a C program to remove all duplicate elements from a 1-D array without using another array.
     int size;
-    printf("Enter the size of array\n");
-    scanf("%d",&size);
-    int arrr[size];
+
+    printf("Enter the size of array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the elements:\n");
+
     for (int i = 0; i < size; i++)
     {
-        scanf("%d",&arrr[i]);
+        scanf("%d", &arr[i]);
     }
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                for (int k = j; k < size - 1; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+
+                size--;
+                j--;
+            }
+        }
+    }
+
+    printf("Array after removing duplicates:\n");
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    
+
     
     
 }
