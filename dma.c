ab6cd3407fb7b5f772621201d67d1d507d48dc34
Arup Barik <arupbarik323@gmail.com>
2026-07-19 01:34:27 +0530

    practice

--- dma.c
+++ dma.c
@@ -1,5 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
+int bubblesort(int n){
+    
+}
+void largest_in_array(int n){
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int large=arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (large<arr[i])
+        {
+            large=arr[i];
+        }
+        else
+        {
+            continue;
+        }
+        
+    }
+    printf("The largest element is :%d\n",large);
+}
+int linearsearch(int n,int target){
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]==target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 int main(){
-printf("Dynamic memory allocation ques\n");
-return 0;
+
+    return 0;
 }
